8119d5e4c1f9a8fdfdcd777dad00439b33ad098b
Fatima Alattas <AlattasFatima22@Gmail.com>
2023-12-18 17:56:52 +0300

    Add files via upload

---
+++ JsonPH.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('#generator');
+const person = document.querySelector('.name');
+const image = document.querySelector('.image');
+const un = document.querySelector('#username');
+const email = document.querySelector('#email');
+const web = document.querySelector('#website');
+const apiURL = 'https://jsonplaceholder.typicode.com/users';
+
+
+
+async function RandomPortfolio(){
+    console.log("dfjnvidx");
+    const res = await fetch(apiURL);
+    const json = await res.json();
+    person.innerText = json[0].name;
+    un.innerText = json[0].username;
+    email.innerText = json[0].email;
+    web.innerText = json[0].website;
+}
+
+
+btn.addEventListener('click',RandomPortfolio);
